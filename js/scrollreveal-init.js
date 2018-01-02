cb41958f2a064dea57ce8ce7872e983624472a90
david <wordburger@gmail.com>
2018-01-01 20:06:06 -0800

    scroll reveal added

---
+++ js/scrollreveal-init.js
@@ -0,0 +1,14 @@
+(function($) {
+	"use strict"; // Start of use strict
+
+	$(document).ready(function() {
+
+		var scrollReveal = ScrollReveal();
+		scrollReveal.reveal("[data-scroll-reveal]",
+		 {
+		 	delay: 200,
+			distance: "5rem"
+		 });
+
+	});
+})(jQuery); // End of use strict
